f607a32d2da318ba285e808d36d5ed49ee9a9c4a
Adam <therefortomorrow63@gmail.com>
2018-10-24 19:12:13 +0300

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>CELT Abroad</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+
+</head>
+<body>
+<header>
+@include('header.header')
+</header>
+
+<footer>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</footer>
+</body>
+</html>
